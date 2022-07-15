e322bcc55412a1d26ac7df01ed4e5cc6788384cd
Ann7D0 <88535888+Ann7D0@users.noreply.github.com>
2022-07-15 09:52:22 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+function check () {
+    var date = document.getElementById("datePicker").value;
+    var logindate = Boolean (date == '1999-07-15');
+    var name = document.getElementById("namePicker").value;
+    var loginname = Boolean (name === 'trần thị xuân');
+    
+    if (logindate == false || loginname == false) {
+        window.location.replace("sorry.html");
+    }
+
+    if (logindate == true  && loginname == true) {
+        window.location.href ="welcome.html"
+    }
+    console.log(loginname);
+    console.log(logindate);
+}
